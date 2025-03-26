959c419f0339acda4fa7622a1329a171ca9b84c5
Elena <pr.splendour@gmail.com>
2025-03-26 12:05:39 +0300

    Вариант 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+new Student("Elena", "female", 19);
+
+new Student("Valentin", "male", 18);
+
+new Student("Teja", "female", 17);
+
+
+
+Student.prototype.setSubject = function(subjectName) {
+	Student.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if ("marks" in Student) {
+		for (let i = 0; i < marks.length, i++) {
+			marks.push(...marks[i]);
+		}
+	} else {
+		console.log("студент отчислен");
+	}
+};
+
+Student.prototype.getAverage = function(...marks) {
+	let marksAverage = 0;
+	if ("marks" in Student === false) {
+		marksAverage = 0;
+	} else {
+		for (let i = 0; i < marks.length, i++) {
+			let marksCount = marks.length - 1;
+			marksAverage = sum(...marks) / marksCount;
+		}
+	}
+
+	return marksAverage;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete Student.marks;
+	delete Student.subject;
+	Student.excluded = reason;
 }
