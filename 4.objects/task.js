e17243646dbe07aec5c7779b0f70a987008787d1
Elena <pr.splendour@gmail.com>
2025-03-27 11:47:02 +0300

    Исправленная версия ДЗ -1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,44 +4,27 @@ function Student(name, gender, age) {
 	this.age = age;
 	this.marks = [];
 }
-new Student("Elena", "female", 19);
-
-new Student("Valentin", "male", 18);
-
-new Student("Teja", "female", 17);
-
-
 
 Student.prototype.setSubject = function(subjectName) {
-	Student.subject = subjectName;
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function(...marks) {
-	if ("marks" in Student) {
-		for (let i = 0; i < marks.length, i++) {
-			marks.push(...marks[i]);
-		}
-	} else {
-		console.log("студент отчислен");
-	}
-};
+Student.prototype.addMarks = function(...marksToAdd) {
+	if ("marks" in thishis) {
+		this.marks.push(...marksToAdd);
+	} 
+}
 
 Student.prototype.getAverage = function(...marks) {
-	let marksAverage = 0;
-	if ("marks" in Student === false) {
-		marksAverage = 0;
-	} else {
-		for (let i = 0; i < marks.length, i++) {
-			let marksCount = marks.length - 1;
-			marksAverage = sum(...marks) / marksCount;
-		}
+	if ("marks" in this === false) {
+         this.marks = 0;
 	}
-
-	return marksAverage;
+	return marks.reduce((a,b) => (a+b)) / marks.length
 }
 
+
 Student.prototype.exclude = function(reason) {
-	delete Student.marks;
-	delete Student.subject;
-	Student.excluded = reason;
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
